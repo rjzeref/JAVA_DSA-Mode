8c533b9efae1b120ceabd1b7ba55bdaf6e5968ef
Roumo Kundu <rjroumo@gmail.com>
2023-07-02 23:53:36 +0530

    new update

--- into.java
+++ into.java
@@ -1,12 +1,18 @@
+class into {
+  int a = 0;
+  int b = 1, c;
 
-class into 
-{
-  public static void main(String args[]) 
-  {
-    for (int i = 1; i <= 10; i++) {
-      System.out.println(i+11);
-    }
-    
+  int fibo(int n) {
+    if (n == 1)
+      return 0;
+    c = a + b;
+    a = b;
+    b = c;
+    return fibo((n - 1));
   }
 
+  public static void main(String args[]) {
+    into ob = new into();
+    System.out.println(ob.fibo(9));
+  }
 }
