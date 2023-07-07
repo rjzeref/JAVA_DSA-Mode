cd5a5a1813fec124e159f39680b1bbac70fea1a2
Roumo Kundu <rjroumo@gmail.com>
2023-07-08 00:28:30 +0530

    new update

--- into.java
+++ into.java
@@ -1,22 +1,28 @@
-import java.util.*;
-class students 
-{
-  String s;
-  int roll;
-  void show()
-  {
-    System.out.println(s+" "+roll);
-  }
-}
+import java.util.Scanner;
 
-class into
-{
+class into {
+  String name;
+  int roll;
+
+  into(String name, int roll) 
+  {
+    this.name = name;
+    this.roll = roll;
+  }
+
   public static void main(String[] args) 
   {
-    students ob = new students();
-    ob.s = "Roumo";
-    ob.roll = 12345;
+    Scanner in = new Scanner(System.in);
+    into ob[] = new into[3];
+    for (int i = 0; i < ob.length; i++) 
+    {
+      System.out.println("Enter Data ");
+      ob[i] = new into(in.next(), in.nextInt());
+    }
     
-    ob.show();
+    for (into o : ob) 
+    {
+      System.out.println(o.name);
+    }
   }
 }
