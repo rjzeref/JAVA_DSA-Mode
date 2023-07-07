e0cf452e7253cf491850def409aa397a5e78cefe
Roumo Kundu <rjroumo@gmail.com>
2023-07-07 21:23:16 +0530

    updated

--- into.java
+++ into.java
@@ -1,18 +1,22 @@
-class into {
-  int a = 0;
-  int b = 1, c;
-
-  int fibo(int n) {
-    if (n == 1)
-      return 0;
-    c = a + b;
-    a = b;
-    b = c;
-    return fibo((n - 1));
+import java.util.*;
+class students 
+{
+  String s;
+  int roll;
+  void show()
+  {
+    System.out.println(s+" "+roll);
   }
+}
 
-  public static void main(String args[]) {
-    into ob = new into();
-    System.out.println(ob.fibo(9));
+class into
+{
+  public static void main(String[] args) 
+  {
+    students ob = new students();
+    ob.s = "Roumo";
+    ob.roll = 12345;
+    
+    ob.show();
   }
 }
